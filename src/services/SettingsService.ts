c8a246e9bd6d8d63ac45a5e36046ddc4c732b9f9
Nathan Nascimento <nathansantos3211@gmail.com>
2021-04-21 23:17:38 -0300

    Terceira aula

---
+++ src/services/SettingsService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository, Repository} from 'typeorm'
+import { Setting } from '../entities/setting'
+import { SettingsRepository} from '../repositories/SettingsRepository'
+
+
+interface ISettingsCreate {
+    chat: boolean,
+    username: string
+}
+
+class SettingsService {
+    private settingsRepository: Repository<Setting>
+
+    constructor() {
+        this.settingsRepository = getCustomRepository(SettingsRepository)
+    }
+
+
+    async create({ chat, username }) {
+    
+        //Select * from settings where username = "username" limit 1;
+        const userAlreadyExists = await this.settingsRepository.findOne({
+            username
+        })
+
+        if(userAlreadyExists) {
+            throw new Error('User Already exists!')
+        }
+
+        const settings = this.settingsRepository.create({
+            chat,
+            username
+        })
+    
+        await this.settingsRepository.save(settings)
+
+        return settings 
+    }
+}
+
+export { SettingsService }
